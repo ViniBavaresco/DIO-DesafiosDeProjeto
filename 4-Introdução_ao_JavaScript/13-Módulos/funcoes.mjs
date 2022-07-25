b0e2819e6bfd39a54f0f70851a8a435c883a3452
ViniBavaresco <vinibava03@gmail.com>
2022-07-25 09:28:29 -0300

    Exercício com módulos

---
+++ 4-Introdução_ao_JavaScript/13-Módulos/funcoes.mjs
@@ -0,0 +1,11 @@
+export function mostraIdade(nome, idade) {
+    return `A idade de ${nome} é ${idade}.`;
+}
+
+export function mostraCidade(nome, cidade) {
+    return `${nome} mora na cidade de ${cidade}.`;
+}
+
+export function mostraHobby(nome, hobby) {
+    return `${nome} gosta de ${hobby}!`;
+}
---
+++ 4-Introdução_ao_JavaScript/13-Módulos/main.mjs
@@ -0,0 +1,11 @@
+import {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+} from './funcoes.mjs';
+
+console.log(mostraIdade('Maria', 20));
+
+console.log(mostraCidade('José', 'Campinas'));
+
+console.log(mostraHobby('Carol', 'ouvir música'));
