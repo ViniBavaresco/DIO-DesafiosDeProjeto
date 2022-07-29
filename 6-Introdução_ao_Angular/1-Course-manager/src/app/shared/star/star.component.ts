f33be8f41ff9c1ce6e441fe8e67156f61f941c7c
ViniBavaresco <vinibava03@gmail.com>
2022-07-29 18:21:16 -0300

    Atualização de Courses manager

--- 6-Introdução_ao_Angular/1-Course-manager/src/app/shared/star/star.component.ts
+++ 6-Introdução_ao_Angular/1-Course-manager/src/app/shared/star/star.component.ts
@@ -1,18 +1,18 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  styleUrls: ['./star.component.css'],
 })
 export class StarComponent implements OnChanges {
-
   constructor() { }
-  @Input() rating: number = 0;
+
+  @Input()
+  rating: number = 0;
   starWidth: number = 0;
 
   ngOnChanges(): void {
-    this.starWidth = this.rating * 74 / 5;
+    this.starWidth = (this.rating * 74) / 5;
   }
-
 }
