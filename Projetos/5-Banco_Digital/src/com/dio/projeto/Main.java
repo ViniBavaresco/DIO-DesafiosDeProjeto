f0b0b8f2301aaabbde1746bc3729c0625192c09e
ViniBavaresco <vinibava03@gmail.com>
2022-08-04 21:45:14 -0300

    Projeto de Banco Digital com Java

---
+++ Projetos/5-Banco_Digital/src/com/dio/projeto/Main.java
@@ -0,0 +1,19 @@
+package com.dio.projeto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Vinicius");
+
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
